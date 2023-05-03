00f3a3e25f01c4d0cc284cc798fdb950a020c4a8
Delgado <rxd718@miami.edu>
2023-05-03 15:45:31 -0400

    work in progress components

---
+++ src/components/pages/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+        <img src="./assets/images/githubLogo.jpg" 
+             alt="Github Portfolio"
+             href="https://github.com/ohNOitsRO/"></img>
+            
+        <img src="./assets/images/envelope.png" 
+             alt="My Email"
+             href="mailto:fake.email@gmail.com"></img>
+
+        <img src="./assets/images/Linkedin.png" 
+             alt="LinkedIn Profile"
+             href="http://www.linkedin.com"></img>
+    </div>
+  );
+}
---
+++ src/components/pages/Project.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+export default function Header() {
+  return (
+    <section class="my-projects" id="projects">
+
+        <p>NBA betHub</p>
+    <img src="./assets/images/NBAbetHub.png" 
+         alt="Work In Progress"
+         id="projectName1"
+         href="https://ohnoitsro.github.io/NBA-betHub/">
+    </img>
+
+    
+        <p>FavSavs</p>
+    <img src="./assets/images/FavSavs.png" 
+         alt="Work In Progress"
+         id="projectName2"
+         href="https://fierce-everglades-98349.herokuapp.com/">
+    </img>
+
+    <p>Project 3</p>
+    <img src="./assets/images/FavSavs.png" 
+         alt="Work In Progress"
+         id="projectName3"
+         href="https://github.com/ohNOitsRO/">
+    </img>
+
+    <p>Project 4</p>
+    <img src="./assets/images/FavSavs.png" 
+         alt="Work In Progress"
+         id="projectName4"
+         href="https://github.com/ohNOitsRO/">
+    </img>
+
+    <p>Project 5</p>
+    <img src="./assets/images/FavSavs.png" 
+         alt="Work In Progress"
+         id="projectName5"
+         href="https://github.com/ohNOitsRO/">
+    </img>
+
+    </section>
+  );
+}
