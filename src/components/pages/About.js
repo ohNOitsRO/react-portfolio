edf0c0146da027144e0648a159808979ab941bd4
Havok <rngdhavok@hotmail.com>
2023-05-12 01:41:49 -0400

    stylizing

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,22 +8,33 @@ export default function About() {
         // <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
         <div className="container-fluid">
         <div className="container row">
+        
+        <div className= "col-md-3"></div>
+
         <div className= "col-md-3">
         <img src={ProfilePic}
              alt="Roland's Profile"
              id ="about-me"
              className="d-block mx-lg-auto img-fluid"></img>
         </div>
-        <div className= "col-md-9">
-            <p className="lead">Miami based Fullstack web designer and developer, 
+        <div className= "col-md-3">
+            <div className= "card">
+            <div className="card-body h-100">
+            <p className="lead">Miami based Fullstack web developer, 
             fluent in HTML, CSS, JavaScript, mySQL, and MERN.    
             100% Computer Geek certified, show binger, and lover of all sports. 
             Simple, efficient, and elegant designs. 
             Proud student of the University of Miami Coding Bootcamp.
             Keep up with me and all my current and upcoming projects here!
-        </p></div></div>
-        
-        
+        </p>
+        </div>
+        </div>
+        </div>
+
+        <div className= "col-md-3"></div>
+
+
+        </div>
         </div>
             
             
