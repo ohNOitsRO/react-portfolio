8c0f756eafb520f62ec38fe9dd3cfc9f2b1d4229
Delgado <rxd718@miami.edu>
2023-05-19 15:37:33 -0400

    resume fixes

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -30,7 +30,7 @@ export default function Resume() {
             <li className="listspace">mySQL</li>
               <strong>SQL</strong> Database Management
             <li className="listspace">MongoDB</li>
-              <strong>Mongoose</strong> for object models
+              <strong>Mongoose</strong> for object models, <strong>NoSQL</strong> Database Management
             <li className="listspace">Express</li>
               <strong>Sequelize</strong> to connect front-end and back-end
             <li className="listspace">React</li>
