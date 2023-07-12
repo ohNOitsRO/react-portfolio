c08eee11351a8029ec4e92ffab6022a883ea3ecc
Delgado <rxd718@miami.edu>
2023-07-12 14:36:10 -0400

    organizing skills logos

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -39,30 +39,84 @@ export default function Resume() {
           <div className="card-body h-100">
           <div className="lead">
             <u>Technical Skills</u>
-            <ul>
+            
 
-            <img src={ResumeDL}
-           alt="Roland's Resume"
+          <img src={Bootstraplogo}
+           alt="Bootstrap Logo"
            className="d-block mx-lg-auto img-fluid"
            style={thumbNail}></img>
 
-            <li className="listspace">CSS</li>
-              <strong>Bootstrap</strong>, <strong>Materialize</strong> for different page designs
-            <li className="listspace">Javascript</li>
-              <strong>jQuery</strong>, <strong>RESTful API</strong> fetching for C.R.U.D
-            <li className="listspace">mySQL</li>
-              <strong>SQL</strong> Database Management
-            <li className="listspace">MongoDB</li>
-              <strong>Mongoose</strong> for object models, <strong>NoSQL</strong> Database Management
-            <li className="listspace">Express</li>
-              <strong>Sequelize</strong> to connect front-end and back-end
-            <li className="listspace">React</li>
-              <strong>Router</strong> for built in page routes, <strong>MUI</strong> for front-end component designs
-            <li className="listspace">Node</li>
-              <strong>Inquirer</strong> for CLI inputs, <strong>dotenv</strong> to hide credentials, <strong>bcrypt</strong> to hash user data for further security, <strong>babel</strong> for easy ES5-to-ES6 integration, <strong>ESlint</strong> for more descriptive, stricter and customizable restrictions, <strong>Webpack</strong> for compressing and bundling applications, <strong>jest</strong> for testing code
-            <li className="listspace">Insomnia/GraphQL</li>
-              Testing databases and routes
-            </ul>
+          <img src={CSS3logo}
+           alt="CSS3 Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={Expresslogo}
+           alt="Express Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={GraphQLlogo}
+           alt="GraphQL Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={Handlebarslogo}
+           alt="Handlebars Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={HTML5logo}
+           alt="HTML5 Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={JQuerylogo}
+           alt="JQuery Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={JSlogo}
+           alt="Javascript Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={Materializelogo}
+           alt="Materialize Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={Mongodblogo}
+           alt="MongoDB Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={MUIlogo}
+           alt="MUI Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={mySQLlogo}
+           alt="mySQL Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={Nodelogo}
+           alt="Node Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={Reactlogo}
+           alt="React Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+
+          <img src={Sequelizelogo}
+           alt="Sequelize Logo"
+           className="d-block mx-lg-auto img-fluid"
+           style={thumbNail}></img>
+            
+            
       </div>
       </div>
       </div>
